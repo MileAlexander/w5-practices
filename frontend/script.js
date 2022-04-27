cde847f29ea03f26235c81b50da5009d7f24bef0
Mile Alexander <milealexander@gmail.com>
2022-04-27 09:04:35 +0200

    hétfő

--- frontend/script.js
+++ frontend/script.js
@@ -17,3 +17,22 @@ function detectGender(person) {
 
 console.log( detectGender(firstPerson) );
 console.log( detectGender(secondPerson) );
+
+
+
+
+function warnTheSheep(queue) {
+    let rqueue = queue.reverse()
+    for (let i = 0; i < rqueue.length; i++) {
+        if (rqueue.slice(i) === "sheep") {
+        continue
+        } else {
+            let n = i-1
+        }
+    }
+    if(queue.slice(-1) === "wolf") {
+      return "Pls go away and stop eating my sheep"
+    }else {
+      return "Oi! Sheep number "+queue.length+"! You are about to be eaten by a wolf!"
+    }
+}
